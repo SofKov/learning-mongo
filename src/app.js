5bee29ea2c950cc9766c6611592d44d2567664c0
Sofia K <sofiakovalevskaia@gmail.com>
2021-03-25 16:20:43 +0000

    set up email validation in person schema

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 require("./db/connection");
 
+const { Person } = require("./models/Person");
 const { Task } = require("./models/Task");
 
 const app = async () => {
-    // const person = new Person({name: "Sofia", email: "sof@kov.com", age: 27, likesPizza: true});
-    // await person.save();
-    const task = new Task({todo: "buy milk", taskComplete: false});
+    try {const person = new Person({name: "Bob", email: "bob@email.com", age: 27, likesPizza: true});
+    await person.save();
+    const task = new Task({todo: "buy bread", taskComplete: true});
     await task.save();
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 app();
