5124dbd3fc91e493b5e23142ef50cdbb311cf762
Sofia K <sofiakovalevskaia@gmail.com>
2021-03-26 10:22:28 +0000

    updated app.js, removed notes

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ const { Person } = require("./models/Person");
 const { Task } = require("./models/Task");
 
 const app = async () => {
-    try {const person = new Person({name: "Bob", email: "bob@email.com", age: 27, likesPizza: true});
-    await person.save();
-    const task = new Task({todo: "buy bread", taskComplete: true});
-    await task.save();
+    try {
+        const person = new Person({name: "Bob", email: "bob@email.com", age: 27, likesPizza: true});
+        await person.save();
+        const task = new Task({todo: "buy bread", taskComplete: true});
+        await task.save();
     } catch (err) {
         console.log(err);
     }
